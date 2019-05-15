40ae91957b5304a6ba8dc6f05ff55110f8289e56
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2019-05-15 10:45:46 +0200

    fixed binary service tests

--- src/test/integration/storage/test_binary_service.py
+++ src/test/integration/storage/test_binary_service.py
@@ -1,6 +1,6 @@
+# pylint: disable=attribute-defined-outside-init,wrong-import-order
 import gc
 import magic
-import unittest
 
 from helperFunctions.config import get_config_for_testing
 from storage.MongoMgr import MongoMgr
@@ -8,13 +8,12 @@ from storage.binary_service import BinaryService
 from storage.db_interface_backend import BackEndDbInterface
 from test.common_helper import create_test_firmware
 
+_, TEST_FO = create_test_firmware()
 
-TEST_FW = create_test_firmware()
 
+class TestBinaryService:
 
-class TestBinaryService(unittest.TestCase):
-
-    def setUp(self):
+    def setup_method(self):
         self.config = get_config_for_testing()
         self.mongo_server = MongoMgr(config=self.config)
         self._init_test_data()
@@ -22,30 +21,30 @@ class TestBinaryService(unittest.TestCase):
 
     def _init_test_data(self):
         self.backend_db_interface = BackEndDbInterface(config=self.config)
-        self.backend_db_interface.add_firmware(TEST_FW)
+        self.backend_db_interface.add_file_object(TEST_FO)
         self.backend_db_interface.shutdown()
 
-    def tearDown(self):
+    def teardown_method(self):
         self.mongo_server.shutdown()
         gc.collect()
 
     def test_get_binary_and_file_name(self):
-        binary, file_name = self.binary_service.get_binary_and_file_name(TEST_FW.get_uid())
-        self.assertEqual(file_name, TEST_FW.file_name, 'file_name not correct')
-        self.assertEqual(binary, TEST_FW.binary, 'invalid result not correct')
+        binary, file_name = self.binary_service.get_binary_and_file_name(TEST_FO.get_uid())
+        assert file_name == TEST_FO.file_name, 'file_name not correct'
+        assert binary == TEST_FO.binary, 'invalid result not correct'
 
     def test_get_binary_and_file_name_invalid_uid(self):
         binary, file_name = self.binary_service.get_binary_and_file_name('invalid_uid')
-        self.assertIsNone(binary, 'should be none')
-        self.assertIsNone(file_name, 'should be none')
+        assert binary is None, 'should be none'
+        assert file_name is None, 'should be none'
 
     def test_get_repacked_binary_and_file_name(self):
-        tar, file_name = self.binary_service.get_repacked_binary_and_file_name(TEST_FW.get_uid())
-        self.assertEqual(file_name, '{}.tar.gz'.format(TEST_FW.file_name), 'file_name not correct')
+        tar, file_name = self.binary_service.get_repacked_binary_and_file_name(TEST_FO.get_uid())
+        assert file_name == '{}.tar.gz'.format(TEST_FO.file_name), 'file_name not correct'
         file_type = magic.from_buffer(tar, mime=True)
-        self.assertEqual(file_type, 'application/gzip', 'file type not tar')
+        assert file_type == 'application/gzip', 'file type not tar'
 
     def test_get_repacked_binary_and_file_name_invalid_uid(self):
         binary, file_name = self.binary_service.get_repacked_binary_and_file_name('invalid_uid')
-        self.assertIsNone(binary, 'should be none')
-        self.assertIsNone(file_name, 'should be none')
+        assert binary is None, 'should be none'
+        assert file_name is None, 'should be none'
