2c55017641c53aee45092d0fa262530e35371053
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2019-05-22 10:36:47 +0200

    integration test fix

--- src/test/integration/scheduler/test_unpack_analyse_and_compare.py
+++ src/test/integration/scheduler/test_unpack_analyse_and_compare.py
@@ -1,12 +1,14 @@
+# pylint: disable=attribute-defined-outside-init,wrong-import-order
 from multiprocessing import Event, Value
 from tempfile import TemporaryDirectory
-from unittest import TestCase, mock
+from unittest import mock
 
 import gc
 
 from helperFunctions.data_conversion import normalize_compare_id
 from helperFunctions.fileSystem import get_test_data_dir
 from helperFunctions.web_interface import ConnectTo
+from objects.file import FileObject
 from objects.firmware import Firmware
 from scheduler.Analysis import AnalysisScheduler
 from scheduler.Compare import CompareScheduler
@@ -18,10 +20,10 @@ from test.common_helper import clean_test_database, get_database_names
 from test.integration.common import MockFSOrganizer, initialize_config
 
 
-class TestFileAddition(TestCase):
+class TestFileAddition:
 
     @mock.patch('unpacker.unpack.FS_Organizer', MockFSOrganizer)
-    def setUp(self):
+    def setup_method(self):
         self._tmp_dir = TemporaryDirectory()
         self._config = initialize_config(self._tmp_dir)
         self.elements_finished_analyzing = Value('i', 0)
@@ -44,7 +46,7 @@ class TestFileAddition(TestCase):
     def trigger_compare_finished_event(self):
         self.compare_finished_event.set()
 
-    def tearDown(self):
+    def teardown_method(self):
         self._compare_scheduler.shutdown()
         self._unpack_scheduler.shutdown()
         self._analysis_scheduler.shutdown()
@@ -56,27 +58,30 @@ class TestFileAddition(TestCase):
         gc.collect()
 
     def test_unpack_analyse_and_compare(self):
-        test_fw_1 = Firmware(file_path='{}/container/test.zip'.format(get_test_data_dir()))
+        test_fo_1 = FileObject(file_path='{}/container/test.zip'.format(get_test_data_dir()))
+        test_fw_1 = Firmware(uid=test_fo_1.get_uid())
         test_fw_1.release_date = '2017-01-01'
-        test_fw_2 = Firmware(file_path='{}/regression_one'.format(get_test_data_dir()))
+
+        test_fo_2 = FileObject(file_path='{}/regression_one'.format(get_test_data_dir()))
+        test_fw_2 = Firmware(uid=test_fo_2.get_uid())
         test_fw_2.release_date = '2017-01-01'
 
-        self._unpack_scheduler.add_task(test_fw_1)
-        self._unpack_scheduler.add_task(test_fw_2)
+        self._unpack_scheduler.add_task(test_fo_1)
+        self._unpack_scheduler.add_task(test_fo_2)
 
         self.analysis_finished_event.wait(timeout=20)
 
         compare_id = normalize_compare_id(';'.join([fw.uid for fw in [test_fw_1, test_fw_2]]))
 
-        self.assertIsNone(self._compare_scheduler.add_task((compare_id, False)), 'adding compare task creates error')
+        assert self._compare_scheduler.add_task((compare_id, False)) is None, 'adding compare task creates error'
 
         self.compare_finished_event.wait(timeout=10)
 
         with ConnectTo(CompareDbInterface, self._config) as sc:
             result = sc.get_compare_result(compare_id)
 
-        self.assertEqual(result['plugins']['Software'], self._expected_result()['Software'])
-        self.assertCountEqual(result['plugins']['File_Coverage']['files_in_common'], self._expected_result()['File_Coverage']['files_in_common'])
+        assert result['plugins']['Software'] == self._expected_result()['Software']
+        assert len(result['plugins']['File_Coverage']['files_in_common']) == len(self._expected_result()['File_Coverage']['files_in_common'])
 
     @staticmethod
     def _expected_result():
